b97506ad24d546a1d29f10b78d6532368db94227
arnissanurmaulidya <70264816+arnissanurmaulidya@users.noreply.github.com>
2020-08-26 19:38:31 +0700

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("nama siswa : arnissa nur maulidya");
+       System.out.println("jenis kelamin : perempuan");
+       System.out.println("alamat : lawang malang jawa timur");
+       System.out.println("jurusan : rekayasa perangkat lunak");
+    }
+    
+}
